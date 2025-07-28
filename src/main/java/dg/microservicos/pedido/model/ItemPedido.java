cb94e49f60c98f0a168f7e66f1abb2344ddeb5ad
ddouglss <ddouglss1999@gmail.com>
2025-07-28 01:38:44 -0300

    feat(model): criando o pedido e itemPedido

---
+++ src/main/java/dg/microservicos/pedido/model/ItemPedido.java
@@ -0,0 +1,58 @@
+package dg.microservicos.pedido.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private int quantidade;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+
+    public ItemPedido(){}
+
+    public ItemPedido(Long id, String nome, int quantidade, Pedido pedido) {
+        this.id = id;
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.pedido = pedido;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+}
---
+++ src/main/java/dg/microservicos/pedido/model/Pedido.java
@@ -0,0 +1,49 @@
+package dg.microservicos.pedido.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private List<ItemPedido> itens;
+
+    public Pedido(){}
+
+    public Pedido(Long id, String descricao, List<ItemPedido> itens) {
+        this.id = id;
+        this.descricao = descricao;
+        this.itens = itens;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemPedido> itens) {
+        this.itens = itens;
+    }
+}
